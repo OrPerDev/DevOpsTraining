0eb3b9173743535863c861df4e8334fe83a4d33a
OrPerDev <peretzorw@gmail.com>
2022-11-13 19:14:50 +0200

    test

--- GITrepo-ex3-or-peretz/or-peretz.java
+++ GITrepo-ex3-or-peretz/or-peretz.java
@@ -1,6 +1,6 @@
 public class Hello{
 	public static void main(String[] args) {
-		for	(int i = 1; i<= 1; i++) {
+		for	(int i = 1; i<= 2; i++) {
 			System.out.println("Or Peretz...." + i);
 		}
 	}
